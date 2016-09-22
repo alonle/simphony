eb374da520cb9ef78133a6312bdf54ac5e8f54bb
Zach Moshe <zachmoshe@gmail.com>
2016-09-23 01:22:44 +0300

    adding offset to subtitles

--- subtitles.py
+++ subtitles.py
@@ -1,26 +1,29 @@
 __author__ = 'Vaio-190X'
 
 import pysrt
+import re
 
-
-def to_ms(t):
+def to_sec(t):
     m,s,micros = t.minute, t.second, t.microsecond
     return ( micros + 1000000*s + 60*1000000*m ) / 1000000
 
 def produce_dict(filename):
+    m = re.search("offset ([\+\-\d.]+)", open(filename, "r", encoding="iso-8859-1").readlines()[0].lower())
+    offset = float(m.group(1)) if m else 0
+    if offset != 0:
+        print("File {} with offset {}".format(filename, offset))    
     srt = pysrt.open(filename, "iso-8859-1")
     start = []
     end = []
     text = []
     for sub in srt:
-        start.append(to_ms(sub.start.to_time()))
-        end.append(to_ms(sub.end.to_time()))
+        start.append(to_sec(sub.start.to_time()) + offset)
+        end.append(to_sec(sub.end.to_time()) + offset)
         text.append(sub.text)
     srt_dict = dict()
     srt_dict['start'] = start
     srt_dict['end'] = end
     srt_dict['text'] = text
-    srt_dict['filename'] = [filename] * len(start)
     return srt_dict
 
 # Usage example: 
