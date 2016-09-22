ba906e2379f899aa990cb2b95fecaf4a87147ece
oferfrid <oferfrid@gmail.com>
2016-09-22 13:03:05 +0300

    add speech features

--- t_2_v/say_fox.py
+++ t_2_v/say_fox.py
@@ -2,11 +2,16 @@ import pyttsx
 
 engine = pyttsx.init()
 voices = engine.getProperty('voices')
-for voice in voices:
-   engine.setProperty('voice', voice.id)  # changes the voice
-   engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
+for i,voice in enumerate(voices):
+    print '{} {} {} {} {}'.format(i,voice.age,voice.gender,voice.id,voice.languages)
+    engine.setProperty('voice', voice.id)  # changes the voice
+    engine.say('The quick brown fox jumped over the lazy dog.')
+    engine.runAndWait()
 
 engine = pyttsx.init()
-engine.setProperty('voice', 60)  # use whatever voice_id you'd like
+engine.setProperty('voice', 15)  # use whatever voice_id you'd like
+# engine.setProperty('female', 30)
+engine.setProperty('rate', 50)# use whatever voice_id you'd like
 engine.say('The quick brown fox jumped over the lazy dog.')
+# engine.runAndWait()
+
